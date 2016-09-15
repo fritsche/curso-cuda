14148eb6c48479a1163a6b4e7c5e2634ebb6ffa6
Giovanna Garcia <ggarciabas@gmail.com>
2016-09-15 17:15:58 -0300

    converter

--- labs/lab4/SharedMemoryColoredImageBlur/ImageBlur.cu
+++ labs/lab4/SharedMemoryColoredImageBlur/ImageBlur.cu
@@ -17,26 +17,58 @@
 
 #define GTX480   480
 #define GTX680   680
-#define GPU     GTX680
-#if GPU == GTX480
+#define MYGPU     GTX680
+#if MYGPU == GTX480
   #define MP    15  // number of mutiprocessors (SMs) in GTX480
   #define GRID1 (MP*2) // GRID size
   #define NT1   768
-#elif GPU == GTX680
+#elif MYGPU == GTX680
   #define MP    8 // number of mutiprocessors (SMs) in GTX680
   #define GRID1 (MP*2)
   #define NT1    1024
 #endif
 
+
+__global__ void rgb2uintKernelSHM (unsigned int* inputImage, unsigned int* outputImage, int imageHeight, int imageWidth) {
+  __shared__ unsigned int sharedInputImage [NT1];
+  unsigned char * ucharInputImage = ( unsigned char *) sharedInputImage;
+
+  // the thread index
+  int index = threadIdx.x + blockIdx.x * blockDim.x;
+
+  char r, g, b;
+
+  // iterate over the tiles
+  for (int i = 0; i < imageHeight*imageWidth/NT1; ++i)
+  {
+    // a thread can be outside the image?
+    if (! index+i*NT1 > imageWidth*imageHeight) {
+      // load the chunk
+      sharedInputImage[index] = inputImage[index+i*NT1];
+    }
+    __syncthreads();
+
+
+    r = ucharInputImage[index+0];
+    g = ucharInputImage[index+1];
+    b = ucharInputImage[index+2];
+    unsigned int v = ((unsigned int)r << 16) + ((unsigned int)g << 8) + (unsigned int)b;
+
+    outputImage[index+i*NT1] = v;
+
+  }
+
+}
+
 int main(int argc, char *argv[]) {
   wbArg_t args;
   int imageWidth;
   int imageHeight;
-  char *inputImageFile;
-  wbImage_t inputImage;
+  char *inputImageFile; // name of the file
+  wbImage_t inputImage; // the image loaded 
   wbImage_t outputImage;
   unsigned char *hostInputImageData;
-  unsigned char *hostOutputImageData;
+  unsigned char *hostOutputImageData; // the final image
   unsigned char *deviceInputImageData;
   unsigned char *deviceOutputImageData;
 
@@ -46,8 +78,6 @@ int main(int argc, char *argv[]) {
 
   inputImage = wbImport(inputImageFile);
 
-  // The input image is in grayscale, so the number of channels
-  // is 1
   imageWidth  = wbImage_getWidth(inputImage);
   imageHeight = wbImage_getHeight(inputImage);
 
@@ -60,7 +90,7 @@ int main(int argc, char *argv[]) {
 
   wbTime_start(GPU, "Doing GPU memory allocation");
   cudaMalloc((void **)&deviceInputImageData,
-             imageWidth * imageHeight * CHANNELS * sizeof(unsigned char));
+             imageWidth * imageHeight * CHANNELS * sizeof(unsigned char) + 3);
   cudaMalloc((void **)&deviceOutputImageData,
              imageWidth * imageHeight * CHANNELS * sizeof(unsigned char));
   wbTime_stop(GPU, "Doing GPU memory allocation");
@@ -72,10 +102,32 @@ int main(int argc, char *argv[]) {
   wbTime_stop(Copy, "Copying data to the GPU");
   ///////////////////////////////////////////////////////
   wbTime_start(Compute, "Doing the computation on the GPU");
-  //@@ INSERT CODE HERE
-  dim3 DimGrid((imageWidth-1)/16 + 1, (imageHeight-1)/16+1, 1);
-  dim3 DimBlock(16, 16, 1);
-  imageBlur<<<DimGrid,DimBlock>>>(deviceInputImageData, deviceOutputImageData, imageWidth, imageHeight );
+  
+  unsigned int * deviceAuxInputImage = (unsigned int *) deviceInputImageData;
+  unsigned int * deviceConvertedImage;
+  unsigned int * hostConvertedImage;
+  //vector = (float *)malloc( n * sizeof(float) ); 
+  hostConvertedImage = (unsigned int *) malloc (imageWidth * imageHeight * CHANNELS * sizeof(unsigned int));
+
+  cudaMalloc((void **)&deviceConvertedImage,
+             imageWidth * imageHeight * CHANNELS * sizeof(unsigned int));
+
+  //(unsigned int* inputImage, unsigned int* outputImage, int imageHeight, int imageWidth) {
+  rgb2uintKernelSHM<<<GRID1, NT1>>> (deviceAuxInputImage, deviceConvertedImage, imageHeight, imageWidth);
+
+  cudaMemcpy(hostConvertedImage, deviceConvertedImage,
+             imageWidth * imageHeight * CHANNELS * sizeof(unsigned char),
+             cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < imageWidth * imageHeight * CHANNELS; ++i)
+  {
+    printf("%d\n", hostConvertedImage[i]);
+  }
+
+  // //@@ INSERT CODE HERE
+  // dim3 DimGrid((imageWidth-1)/16 + 1, (imageHeight-1)/16+1, 1);
+  // dim3 DimBlock(16, 16, 1);
+  // imageBlur<<<DimGrid,DimBlock>>>(deviceInputImageData, deviceOutputImageData, imageWidth, imageHeight );
 
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
